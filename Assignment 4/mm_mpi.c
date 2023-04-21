77ece302dd93bdf6d6f764e7279d3d9e7285d631
Vishesh Rangwani <vishesh20155@iiitd.ac.in>
2023-04-21 14:59:51 +0000

    Non blocking calls implemented

--- Assignment 4/mm_mpi.c
+++ Assignment 4/mm_mpi.c
@@ -76,16 +76,27 @@ int main(int argc, char *argv[])
         // Master sends the Matrices A, B & C
         for (int r = 1; r < np; ++r)
         {
-            MPI_Send(A + getStart(np, r), N*getBatch(np, r), MPI_INT, r, TAG1, MPI_COMM_WORLD);
-            MPI_Send(B, N * N, MPI_INT, r, TAG2, MPI_COMM_WORLD);
+            // Non-Blocking send
+            MPI_Request req1, req2;
+            MPI_Isend(A + getStart(np, r), N*getBatch(np, r), MPI_INT, r, TAG1, MPI_COMM_WORLD, &req1);
+            MPI_Isend(B, N*N, MPI_INT, r, TAG2, MPI_COMM_WORLD, &req2);
+
+            // Code for BLOCKING send Commented:
+            // MPI_Send(A + getStart(np, r), N*getBatch(np, r), MPI_INT, r, TAG1, MPI_COMM_WORLD);
+            // MPI_Send(B, N * N, MPI_INT, r, TAG2, MPI_COMM_WORLD);
         }
     }
     else    // Processors other than Master receive A&B
     {
         // Do an non-blocking receive here
-        MPI_Status stats1, stats2;
-        MPI_Recv(A, batchSize * N, MPI_INT, 0, TAG1, MPI_COMM_WORLD, &stats1);
-        MPI_Recv(B, N * N, MPI_INT, 0, TAG2, MPI_COMM_WORLD, &stats2);
+        MPI_Request req[2];
+        MPI_Irecv(A, batchSize * N, MPI_INT, 0, TAG1, MPI_COMM_WORLD, &req[0]);
+        MPI_Irecv(B, N * N, MPI_INT, 0, TAG2, MPI_COMM_WORLD, &req[1]);
+
+        // Commented code for BLOCKING recv
+        // MPI_Status stats1, stats2;
+        // MPI_Recv(A, batchSize * N, MPI_INT, 0, TAG1, MPI_COMM_WORLD, &stats1);
+        // MPI_Recv(B, N * N, MPI_INT, 0, TAG2, MPI_COMM_WORLD, &stats2);
 
         // Initialize C while receive is Non-Blocking
         for(int i=0; i<batchSize; ++i) 
@@ -97,6 +108,8 @@ int main(int argc, char *argv[])
         }
 
         // Use wait before starting computation
+        MPI_Status stats[2];
+        MPI_Waitall(2, req, stats);
     }
 
     
@@ -128,19 +141,28 @@ int main(int argc, char *argv[])
     // Send the result to Master from other threads:
     if(rank > 0) 
     {
-        // Send NON BLOCKING here
-        MPI_Send(C, batchSize*N, MPI_INT, 0, TAG3, MPI_COMM_WORLD);
+        // NON BLOCKING Send:
+        MPI_Request req;
+        MPI_Isend(C, batchSize*N, MPI_INT, 0, TAG3, MPI_COMM_WORLD, &req);
+
+        // Commented BLOCKING code:
+        // MPI_Send(C, batchSize*N, MPI_INT, 0, TAG3, MPI_COMM_WORLD);
     }
     else    // Receive in Master
     {
-        // Receive NON-BLOCKING here
+        MPI_Request reqs[np-1];
         for(int r=1; r<np; ++r)
         {
-            MPI_Status stats;
-            MPI_Recv(C + getStart(np, r), getBatch(np, r)*N, MPI_INT, r, TAG3, MPI_COMM_WORLD, &stats);
+            // NON-BLOCKING recv:
+            MPI_Irecv(C + getStart(np, r), getBatch(np, r)*N, MPI_INT, r, TAG3, MPI_COMM_WORLD, &reqs[r-1]);
+            // BLOCKING recv commented
+            // MPI_Status stats;
+            // MPI_Recv(C + getStart(np, r), getBatch(np, r)*N, MPI_INT, r, TAG3, MPI_COMM_WORLD, &stats);
         }
 
         // Put a WaitAll here
+        MPI_Status stats[np-1];
+        MPI_Waitall(np-1, reqs, stats);
 
         end_timer = get_usecs();
         double dur = ((double)(end_timer-start_timer))/1000000;
