f9e4263ed534e389fe7a7e65b734e45591e1c194
Vishesh <vishesh20155@iiitd.ac.in>
2023-04-23 02:28:05 +0530

    Blocking calls in MatMul with no speedup

--- Assignment 4/mm_mpi.c
+++ Assignment 4/mm_mpi.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-#include <mpi.h>
 #include <stdlib.h>
+#include <mpi.h>
 #include <sys/time.h>
-#include <string.h>
 #include <assert.h>
 
 int N = 1024;
-int TAG1 = 17, TAG2 = 45, TAG3 = 18;
+int TAG1 = 0, TAG2 = 2000, TAG3 = 4000;
 
 // Min implementation for int parameters
-long min(int a, int b)
+int min(int a, int b)
 {
-  return a < b ? a : b;
+    return a < b ? a : b;
 }
 
 long get_usecs () {
-  struct timeval t;
-  gettimeofday(&t,NULL);
-  return t.tv_sec*1000000+t.tv_usec;
+    struct timeval t;
+    gettimeofday(&t,NULL);
+    return t.tv_sec*1000000+t.tv_usec;
 }
 
 int getBatch(int d, int rank)
@@ -34,17 +33,20 @@ int getBatch(int d, int rank)
 // Function to compute the starting index for inner loop for current rank
 int getStart(int d, int rank)
 {
-  int batch = N / (d);
-  int start = batch * (rank);
-  int extra = N % (d);
-  start += min(rank, extra);
-  return start;
+    int batch = N / (d);
+    int start = batch * (rank);
+    int extra = N % (d);
+    start += min(rank, extra);
+    return start;
 }
+
 
 int main(int argc, char *argv[])
 {
-    if (argc > 1)
+    if(argc > 1) 
+    {
         N = atoi(argv[1]);
+    }
 
     int rank, np;
 
@@ -54,125 +56,146 @@ int main(int argc, char *argv[])
 
     int batchSize = getBatch(np, rank);
     int startIndex = getStart(np, rank);
-    // printf("Batch Size %d and Starting row %d in rank %d\n", batchSize, startIndex, rank);
+    int numRows = ( (rank == 0) ? N : batchSize );
 
-    // Efficient Allocation of A & C in each proc:
-    int A[rank == 0 ? N : batchSize][N], B[N][N], C[rank == 0 ? N : batchSize][N];
-    int start_timer, end_timer;
-    // Initialization of A & B in Master thread:
-    if (rank == 0)
+    // printf("Start Index %d | Batch Size %d | Rank %d\n", startIndex, batchSize, rank);
+
+    // Efficient declaration of A in each rank
+    int **A = (int **)malloc(numRows * sizeof(int *));
+    for(int i=0; i<numRows; ++i)
     {
-        for (int i = 0; i < N; ++i)
-        {
-            for (int j = 0; j < N; ++j)
-            {
+        A[i] = (int *)malloc(N * sizeof(int));
+    }
+
+    int **B = (int **)malloc(N * sizeof(int *));
+    for(int i=0; i<N; ++i) 
+    {
+        B[i] = (int *)malloc(N * sizeof(int));
+    }
+
+    // Allocating C efficiently -- only required number of rows are allocated
+    int **C = (int **)malloc(numRows * sizeof(int *));
+    for(int i=0; i<numRows; ++i)
+    {
+        C[i] = (int *)malloc(N * sizeof(int));
+    }
+
+    long start_time, end_time;
+
+
+    if(rank == 0)
+    {
+        // Initializing A & B in Master
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 A[i][j] = 1;
                 B[i][j] = 1;
-                C[i][j] = 0;
             }
         }
 
-        start_timer = get_usecs();
-        // Master sends the Matrices A, B & C
-        for (int r = 1; r < np; ++r)
+        start_time = get_usecs();
+        for(int rnk=1; rnk<np; ++rnk)
         {
-            // Non-Blocking send
-            MPI_Request req1, req2;
-            MPI_Isend(A + getStart(np, r), N*getBatch(np, r), MPI_INT, r, TAG1, MPI_COMM_WORLD, &req1);
-            MPI_Isend(B, N*N, MPI_INT, r, TAG2, MPI_COMM_WORLD, &req2);
-
-            // Code for BLOCKING send Commented:
-            // MPI_Send(A + getStart(np, r), N*getBatch(np, r), MPI_INT, r, TAG1, MPI_COMM_WORLD);
-            // MPI_Send(B, N * N, MPI_INT, r, TAG2, MPI_COMM_WORLD);
+            int rankStart = getStart(np, rnk);
+            int rankBatch = getBatch(np, rnk);
+            for(int i=0; i<rankBatch; ++i)
+                MPI_Send(A[i+rankStart], N, MPI_INT, rnk, TAG1 + i, MPI_COMM_WORLD);
         }
     }
-    else    // Processors other than Master receive A&B
+    else 
     {
-        // Do an non-blocking receive here
-        MPI_Request req[2];
-        MPI_Irecv(A, batchSize * N, MPI_INT, 0, TAG1, MPI_COMM_WORLD, &req[0]);
-        MPI_Irecv(B, N * N, MPI_INT, 0, TAG2, MPI_COMM_WORLD, &req[1]);
-
-        // Commented code for BLOCKING recv
-        // MPI_Status stats1, stats2;
-        // MPI_Recv(A, batchSize * N, MPI_INT, 0, TAG1, MPI_COMM_WORLD, &stats1);
-        // MPI_Recv(B, N * N, MPI_INT, 0, TAG2, MPI_COMM_WORLD, &stats2);
-
-        // Initialize C while receive is Non-Blocking
-        for(int i=0; i<batchSize; ++i) 
-        {
-            for(int j=0; j<N; ++j)
-            {
-                C[i][j] = 0;
-            }
-        }
-
-        // Use wait before starting computation
-        MPI_Status stats[2];
-        MPI_Waitall(2, req, stats);
+        MPI_Status stat[batchSize];
+        for(int i=0; i<batchSize; ++i)
+            MPI_Recv(A[i], N, MPI_INT, 0, TAG1 + i, MPI_COMM_WORLD, &stat[i]);
     }
 
-    
-    // Computing MatMul here:
-    for(int i=0; i<batchSize; ++i) 
+    // Get value of B using Broadcasr
+    for(int i=0; i<N; ++i)
+        MPI_Bcast(B[i], N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Computing Matmul in C in each rank:
+    for(int i=0; i<numRows; ++i)
     {
-        for(int j=0; j<N; ++j) 
+        for(int j=0; j<N; ++j)
         {
-            for(int k=0; k<N; ++k) 
+            C[i][j] = 0;
+        }
+    }
+
+    // for(int i=0; i<N; ++i) 
+    // {
+    //     for(int j=0; j<N; ++j) 
+    //         printf("%d ", B[i][j]);
+    //     printf("| row %d | rank %d --> B\n", i, rank);
+    // }
+
+
+    // for (int i = 0; i < numRows; i++)
+    // {
+    //     for (int j = 0; j < N; j++)
+    //         printf("%d ", A[i][j]);
+    //     printf("| row %d | rank %d \n", i, rank);
+    // }
+
+    for(int i=0; i<numRows; ++i)
+    {
+        for(int j=0; j<N; ++j)
+        {
+            for(int k=0; k<N; ++k)
             {
                 C[i][j] += (A[i][k] * B[k][j]);
             }
         }
-    }
 
-/*  // For printing inside a proc
-
-    printf("Inside Proc %d Matrix C:\n", rank);
-    for (int i = 0; i < batchSize; ++i)
-    {
-        for (int j = 0; j < N; ++j)
+        if(rank != 0)
         {
-            printf("%d ", C[i][j]);
+            MPI_Send(C[i], N, MPI_INT, 0, TAG3+i, MPI_COMM_WORLD);
+            // MPI_Request req;
+            // MPI_Isend(C[i], N, MPI_INT, 0, TAG3+i, MPI_COMM_WORLD, &req);
         }
-        printf("\t:%d\n", rank);
     }
-*/
 
-    // Send the result to Master from other threads:
-    if(rank > 0) 
+    if (rank == 0) 
     {
-        // NON BLOCKING Send:
-        MPI_Request req;
-        MPI_Isend(C, batchSize*N, MPI_INT, 0, TAG3, MPI_COMM_WORLD, &req);
-
-        // Commented BLOCKING code:
-        // MPI_Send(C, batchSize*N, MPI_INT, 0, TAG3, MPI_COMM_WORLD);
-    }
-    else    // Receive in Master
-    {
-        MPI_Request reqs[np-1];
-        for(int r=1; r<np; ++r)
+        // Master (root) Receiving the data in a Non-Blocking Call
+        for(int rnk=1; rnk<np; ++rnk)
         {
-            // NON-BLOCKING recv:
-            MPI_Irecv(C + getStart(np, r), getBatch(np, r)*N, MPI_INT, r, TAG3, MPI_COMM_WORLD, &reqs[r-1]);
-            // BLOCKING recv commented
-            // MPI_Status stats;
-            // MPI_Recv(C + getStart(np, r), getBatch(np, r)*N, MPI_INT, r, TAG3, MPI_COMM_WORLD, &stats);
+            int rankStart = getStart(np, rnk);
+            int rankBatch = getBatch(np, rnk);
+            MPI_Status stats[rankBatch];
+            for(int i=0; i<rankBatch; ++i)
+                MPI_Recv(C[rankStart+i], N, MPI_INT, rnk, TAG3+i, MPI_COMM_WORLD, &stats[i]);
         }
+        
+        // Stopping the timer:
+        end_time = get_usecs();
+        double dur = ((double)(end_time-start_time))/1000000;
 
-        // Put a WaitAll here
-        MPI_Status stats[np-1];
-        MPI_Waitall(np-1, reqs, stats);
-
-        end_timer = get_usecs();
-        double dur = ((double)(end_timer-start_timer))/1000000;
-        // Validating the output, checking against values as in Assignment 1:
+        // Validating the output
         for(int i=0; i<N; i++) for(int j=0; j<N; j++) assert(C[i][j] == N);
         printf("Test Success. \n");
         printf("Time = %.3f\n", dur);
     }
+
+    // for (int i = 0; i < numRows; i++)
+    // {
+    //     for (int j = 0; j < N; j++)
+    //         printf("%d ", C[i][j]);
+    //     printf("| row %d | rank %d \n", i, rank);
+    // }
     
+    // A[0][0] = 4;
+    // B[0][0] = 4;
+    // C[0][0] = 4;
+
+    // Free Up the allocated memory space
+    // for(int i=0; i<N; ++i) free(A[i]);
+	free(A);
+    free(B);
+    free(C);
     MPI_Finalize();
 
-    return 0;
+
+	return 0;
 }
+
